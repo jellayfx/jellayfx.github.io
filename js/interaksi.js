a1091ba946dcdb1791ea8c525eaea3b3834de6bf
jellayfx <jellayfx@gmail.com>
2025-11-12 15:38:42 +0700

    Update interaksi.js

--- js/interaksi.js
+++ js/interaksi.js
@@ -1,21 +1,13 @@
-// File: interaksi.js
-
 document.getElementById('tampilkan-instagram-btn').addEventListener('click', function(e) {
-    // Mencegah browser mengikuti tautan '#' 
     e.preventDefault(); 
-    
-    // Mendapatkan elemen wadah galeri dan elemen teks tombol
     var galeriWadah = document.getElementById('galeri-instagram-tersembunyi');
     var buttonText = document.getElementById('button-text-toggle');
     
-    // Toggle class 'tersembunyi'
     if (galeriWadah.classList.contains('tersembunyi')) {
-        // TAMPILKAN: Hapus class 'tersembunyi'
         galeriWadah.classList.remove('tersembunyi');
-        buttonText.textContent = 'Sembunyikan Galeri Instagram'; // Ubah teks tombol
+        buttonText.textContent = 'Sembunyikan Galeri Instagram';
     } else {
-        // SEMBUNYIKAN: Tambahkan class 'tersembunyi'
         galeriWadah.classList.add('tersembunyi');
-        buttonText.textContent = 'Lihat Galeri Instagram'; // Ubah teks kembali
+        buttonText.textContent = 'Lihat Galeri Instagram';
     }
 });
