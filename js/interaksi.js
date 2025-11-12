046c96df6188410294f29c9dd1a44b6b84e119fe
jellayfx <jellayfx@gmail.com>
2025-11-12 09:40:38 +0700

    Update interaksi.js

--- js/interaksi.js
+++ js/interaksi.js
@@ -35,3 +35,23 @@ document.getElementById('tampilkan-gopay-coffee').addEventListener('click', func
         this.textContent = 'Lihat Qris';
     }
 });
+
+
+// File: interaksi.js
+
+document.getElementById('tampilkan-instagram-btn').addEventListener('click', function(e) {
+    e.preventDefault(); 
+    
+    var galeriWadah = document.getElementById('galeri-instagram-tersembunyi');
+    var buttonText = document.getElementById('button-text-toggle'); // Menargetkan span teks tombol
+    
+    if (galeriWadah.classList.contains('tersembunyi')) {
+        // Jika tersembunyi, TAMPILKAN
+        galeriWadah.classList.remove('tersembunyi');
+        buttonText.textContent = 'Sembunyikan Galeri Instagram'; // Ubah teks di dalam span
+    } else {
+        // Jika terlihat, SEMBUNYIKAN
+        galeriWadah.classList.add('tersembunyi');
+        buttonText.textContent = 'Lihat Galeri Instagram'; // Ubah teks kembali
+    }
+});
