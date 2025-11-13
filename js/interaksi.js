b840c533e9ba7928a33c5abffcad22388935a487
jellayfx <jellayfx@gmail.com>
2025-11-13 15:06:02 +0700

    Update interaksi.js

--- js/interaksi.js
+++ js/interaksi.js
@@ -1,13 +1,13 @@
-document.getElementById('tampilkan-instagram-btn').addEventListener('click', function(e) {
+document.getElementById('tampilkan-qris-gopay-btn').addEventListener('click', function(e) {
     e.preventDefault(); 
-    var galeriWadah = document.getElementById('galeri-instagram-tersembunyi');
+    var galeriWadah = document.getElementById('galeri-qris-gopay-tersembunyi');
     var buttonText = document.getElementById('button-text-toggle');
     
     if (galeriWadah.classList.contains('tersembunyi')) {
         galeriWadah.classList.remove('tersembunyi');
-        buttonText.textContent = 'Sembunyikan Galeri Instagram';
+        buttonText.textContent = 'Sembunyikan Qris Gopay';
     } else {
         galeriWadah.classList.add('tersembunyi');
-        buttonText.textContent = 'Lihat Galeri Instagram';
+        buttonText.textContent = 'Lihat Qris Gopay';
     }
 });
