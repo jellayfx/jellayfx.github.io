09c91a2f7f94d69f125dec3cfa630ebaf57cd19e
jellayfx <jellayfx@gmail.com>
2025-11-17 12:18:50 +0700

    Create waktu.js

---
+++ js/waktu.js
@@ -0,0 +1,18 @@
+var date = new Date();
+var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+var hours = date.getHours();
+var minutes = date.getMinutes();
+
+// Tambahkan awalan nol jika jam atau menit kurang dari 10
+if (hours < 10) { hours = "0" + hours; }
+if (minutes < 10) { minutes = "0" + minutes; }
+
+var day = days[date.getDay()];
+var dateNum = date.getDate();
+var month = months[date.getMonth()];
+var year = date.getFullYear();
+
+console.log(hours + "." + minutes + " WIB - " + day + ", " + dateNum + " " + month + " " + year);
+
+var element = document.querySelector("body"); var watermark = document.createElement("div");
